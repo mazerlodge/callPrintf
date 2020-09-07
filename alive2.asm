75a7cd9cd4cef0ff7071d23c0eaaadf978c31ee9
mazerlodge <mazerlodge@hotmail.com>
2020-09-07 17:50:31 -0500

    added code with multiple string format specifiers.

---
+++ alive2.asm
@@ -0,0 +1,46 @@
+; alive2.asm
+section .data
+	msg1	db	"Hello, world!", 0
+	msg2	db	"Alive and kicking!", 0
+	radius	dd	357
+	pi	dq	3.14
+	fmtstr	db	"%s", 10, 0
+	fmtflt	db	"%lf", 10, 0
+	fmtint	db	"%d", 10, 0
+
+section .bss
+section .text
+extern	printf
+	global main
+main:
+	push 	rbp
+	mov	rbp, rsp
+
+	; print msg1
+	mov	rax, 0		; no floating point 
+	mov	rdi, fmtstr
+	mov	rsi, msg1
+	call	printf
+
+	; print msg2
+	mov	rax, 0 		; no floating point
+	mov	rdi, fmtstr
+	mov 	rsi, msg2
+	call	printf
+
+	; print radius
+	mov	rax, 0 		; no floating point
+	mov	rdi, fmtint
+	mov	rsi, [radius]
+	call	printf
+
+	; print pi 
+	mov	rax, 1 		; 1 xmm register used
+	movq	xmm0, [pi]
+	mov	rdi, fmtflt
+	call	printf
+
+	mov	rsp, rbp
+	pop	rbp
+
+	ret
